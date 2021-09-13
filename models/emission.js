36df9b8d7b8d543506f305e856b208f6c44249a6
Folivi Joshua Oluwaseun <joshua.folivi@gmail.com>
2021-09-13 11:17:33 +0100

    added model for emission model

--- models/emission.js
+++ models/emission.js
@@ -1,7 +1,36 @@
 module.exports = (sequelize, Sequelize) => {
     const Emission = sequelize.define('emissions', {
+        id: {
+            allowNull: false,
+            primaryKey: true,
+            type: Sequelize.UUID,
+            defaultValue: Sequelize.UUIDV4
+        },
 
+        productId: {
+            type: Sequelize.UUID,
+            defaultValue: Sequelize.UUIDV4,
+            allowNull: false,
+        },
+
+        emission: {
+            type: Sequelize.FLOAT,
+        },
+
+        recordedAt: {
+            type: Sequelize.DATE,
+            defaultValue: null,
+            allowNull: true,
+        }
+    }, {
+        paranoid: true,
+        timestamps: true
     })
 
+    Emission.prototype.toJSON = function () {
+        let values = Object.assign({}, this.get());
+
+        return values;
+    }
     return Emission;
 }
