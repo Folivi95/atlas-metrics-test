94143688214504859f43b41ac0f5a6176ca6f56c
Folivi Joshua Oluwaseun <joshua.folivi@gmail.com>
2021-09-13 14:27:17 +0100

    check for erroneous inputs

--- server/controllers/emissionController.js
+++ server/controllers/emissionController.js
@@ -1,6 +1,7 @@
 const db = require('../../models/index');
 const Emission = db.emissions;
 const AppError = require('../../config/appError');
+const moment = require('moment');
 
 exports.create = async (req, res, next) => {
     try {
@@ -11,6 +12,20 @@ exports.create = async (req, res, next) => {
             emission,
             recordedAt
         };
+
+        let request = ['productId', 'emission', 'recordedAt'];
+        // check if request item is passed in request body
+        request.map(item => {
+            if (!req.body[item]) return next(new AppError(`${item} is required`, 400));
+        })
+
+        if (emission && (isNaN(emission) || emission === '')) {
+            return next(new AppError('Emission should be a number', 400));
+        }
+
+        if (recordedAt && moment(recordedAt, "DD/MM/YYYY", true).isValid()) {
+            return next(new AppError('RecordedAt date should be in the format DD/MM/YYYY', 400));
+        }
 
         // convert recordedAt to date
         recordedAt = new Date(recordedAt).toISOString();
