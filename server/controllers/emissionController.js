33af5242705015c51fb16af3a118dea6f04e0ee7
Folivi Joshua Oluwaseun <joshua.folivi@gmail.com>
2021-09-13 13:04:34 +0100

    implemented emission controller

--- server/controllers/emissionController.js
+++ server/controllers/emissionController.js
@@ -1,9 +1,38 @@
-
+const db = require('../../models/index');
+const Emission = db.emissions;
+const AppError = require('../../config/appError');
 
 exports.create = async (req, res, next) => {
     try {
+        let {productId, emission, recordedAt} = req.body;
 
+        let newEmission = {
+            productId,
+            emission,
+            recordedAt
+        };
+
+        // convert recordedAt to date
+        recordedAt = new Date(recordedAt).toISOString();
+
+        const createdEmission = await Emission.create(newEmission);
+
+        if (!createdEmission) {
+            return next(new AppError('Failed to create emission', 500));
+        }
+
+        let resp = {
+            code: 201,
+            status: 'success',
+            message: 'Emission created successfully',
+            data: createdEmission
+        }
+
+        res.status(resp.code).json(resp);
+        res.locals.resp = resp;
+        return next();
     } catch (err) {
-        
+        console.error('Create Emission Error: ', err);
+        return next(err);
     }
 }
--- server/controllers/statController.js
+++ server/controllers/statController.js
@@ -3,6 +3,7 @@ exports.get = async (req, res, next) => {
     try {
 
     } catch (err) {
-
+        console.error('Get Stats Error: ', err);
+        return next(err);
     }
 }
